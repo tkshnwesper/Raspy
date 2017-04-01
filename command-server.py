ad35d326ebff7711be9d17e375e0d820053d2e49
Saurabh <unknown7bolt@gmail.com>
2017-04-01 13:26:12 +0530

    LOL

--- command-server.py
+++ command-server.py
@@ -1,22 +1,46 @@
-''' Executes the command '''
-
-import subprocess
+'''
+Module which acts as a server, and processes the requests of clients
+'''
+import uuid
 import os
+import subprocess
 import cherrypy
 
 IMAGE_DIRECTORY = "server-image"
-DOCKER_COMMAND = "python /tf_files/label_image.py /tf_files/Raspy/server-image/{}"
+# DOCKER_COMMAND = "dir"
 
-class Command(object):
+class Server(object):
+    ''' Server class '''
     @cherrypy.expose
-    def index(self, imgname):
-        ''' executes command '''
-        result = subprocess.check_output(DOCKER_COMMAND.format(imgname), shell=True)
-        ## delete file
-        # fname = '{}/{}'.format(IMAGE_DIRECTORY, imgname)
+    def index(self):
+        ''' Serves /index path '''
+        return "Welcome to the FYP Server. POST /analyse [image file] : plain/text"
+
+    @cherrypy.expose
+    def analyse(self, img):
+        ''' Serves /analyse path '''
+        imgname = uuid.uuid4().hex + '.jpeg'
+        all_data = bytearray()
+        # check if directory exists, if not creates one
+        if not os.path.exists(IMAGE_DIRECTORY):
+            os.makedirs(IMAGE_DIRECTORY)
+        # image will be saved with name IMAGE_DIRECTORY/id
+        fname = os.path.join(IMAGE_DIRECTORY, imgname)
+        _file = open(fname, "wb")
+        while True:
+            data = img.file.read(8192)
+            all_data += data
+            if not data:
+                break
+        # write image data to file
+        _file.write(all_data)
+        _file.close()
+        # run docker command
+        # result = subprocess.check_output(DOCKER_COMMAND, shell=True)
+        # delete file
         # if os.path.exists(fname):
         #     os.remove(fname)
-        return result
+        return imgname
 
 if __name__ == "__main__":
     from cherrypy.process import servers
@@ -27,7 +51,7 @@ if __name__ == "__main__":
         'global': {
             'server.socket_host': '0.0.0.0',
             'server.socket_timeout' : 60,
-            'server.socket_port': 9090
+            'server.socket_port': 8080
         }
     }
-    cherrypy.quickstart(Command(), '/', config)
+    cherrypy.quickstart(Server(), '/', config)
--- server.py
+++ server.py
@@ -1,46 +1,22 @@
-'''
-Module which acts as a server, and processes the requests of clients
-'''
-import uuid
-import os
+''' Executes the command '''
+
 import subprocess
+import os
 import cherrypy
 
 IMAGE_DIRECTORY = "server-image"
-# DOCKER_COMMAND = "dir"
+DOCKER_COMMAND = "python /tf_files/label_image.py /tf_files/Raspy/server-image/{}"
 
-class Server(object):
-    ''' Server class '''
+class Command(object):
     @cherrypy.expose
-    def index(self):
-        ''' Serves /index path '''
-        return "Welcome to the FYP Server. POST /analyse [image file] : plain/text"
-
-    @cherrypy.expose
-    def analyse(self, img):
-        ''' Serves /analyse path '''
-        imgname = uuid.uuid4().hex + '.jpeg'
-        all_data = bytearray()
-        # check if directory exists, if not creates one
-        if not os.path.exists(IMAGE_DIRECTORY):
-            os.makedirs(IMAGE_DIRECTORY)
-        # image will be saved with name IMAGE_DIRECTORY/id
-        fname = os.path.join(IMAGE_DIRECTORY, imgname)
-        _file = open(fname, "wb")
-        while True:
-            data = img.file.read(8192)
-            all_data += data
-            if not data:
-                break
-        # write image data to file
-        _file.write(all_data)
-        _file.close()
-        # run docker command
-        # result = subprocess.check_output(DOCKER_COMMAND, shell=True)
-        # delete file
+    def index(self, imgname):
+        ''' executes command '''
+        result = subprocess.check_output(DOCKER_COMMAND.format(imgname), shell=True)
+        ## delete file
+        # fname = '{}/{}'.format(IMAGE_DIRECTORY, imgname)
         # if os.path.exists(fname):
         #     os.remove(fname)
-        return imgname
+        return result
 
 if __name__ == "__main__":
     from cherrypy.process import servers
@@ -51,7 +27,7 @@ if __name__ == "__main__":
         'global': {
             'server.socket_host': '0.0.0.0',
             'server.socket_timeout' : 60,
-            'server.socket_port': 8080
+            'server.socket_port': 9090
         }
     }
-    cherrypy.quickstart(Server(), '/', config)
+    cherrypy.quickstart(Command(), '/', config)
