06b194ae5ea3cd4e348745a39c810544ad4f0e30
Saurabh <unknown7bolt@gmail.com>
2017-04-02 14:19:18 +0530

    changed board to bcm

--- lcd.py
+++ lcd.py
@@ -1,8 +1,10 @@
 from RPLCD import CharLCD
+from RPi import GPIO
 
 class LCD(object):
     def __init__(self):
-        self.lcd = CharLCD(rows=2, cols=16)
+        self.lcd = CharLCD(pin_rs=22, pin_rw=24, pin_e=23, pins_data=[9,25,11,8],
+        numbering_mode=GPIO.BCM, rows=2, cols=16)
     
     def write(self, text):
         self.lcd.write_string(text)
