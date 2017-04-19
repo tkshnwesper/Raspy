177e862da18f7a637e3f7cb7631f348ed931524b
Saurabh <unknown7bolt@gmail.com>
2017-04-19 14:39:58 +0530

    whew these new changes

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ import sys
 import threading
 import time
 import cherrypy
-from label_image1 import process_image
+import tensorflow as tf
 
 IMAGE_DIRECTORY = "server-image"
 # DOCKER_COMMAND = "python /tf_files/label_image1.py /tf_files/Raspy/server-image/{}"
@@ -18,12 +18,20 @@ class ThreadLoop(threading.Thread):
     def run(self):
         global JOBS, COMPLETED_JOBS
         from label_image1 import process_image
-        while(True):
-            if len(JOBS) == 0:
-                time.sleep(1)
-            else:
-                fname = JOBS.pop()
-                COMPLETED_JOBS[fname] = process_image(fname)
+        # Unpersists graph from file
+        f = tf.gfile.FastGFile("/home/nitin/tf_files/retrained_graph.pb", 'rb')
+        graph_def = tf.GraphDef()
+        graph_def.ParseFromString(f.read())
+        _ = tf.import_graph_def(graph_def, name='')
+        label_lines = [line.rstrip() for line
+                       in tf.gfile.GFile("/home/nitin/tf_files/retrained_labels.txt")]
+        with tf.Session() as sess:
+            while(True):
+                if len(JOBS) == 0:
+                    time.sleep(1)
+                else:
+                    fname = JOBS.pop()
+                    COMPLETED_JOBS[fname] = process_image(fname, label_lines, sess)
 
             
 
@@ -38,16 +46,19 @@ class Command(object):
         if self.firstTime:
             ThreadLoop().start()
             self.firstTime = False
+            # time.sleep(5)
         # result = subprocess.check_output(DOCKER_COMMAND.format(imgname), shell=True)
         ## delete file
-        fname = '{}/{}'.format(IMAGE_DIRECTORY, imgname)
+        # fname = '{}/{}'.format(IMAGE_DIRECTORY, imgname)
+        fname = 'img.jpg'
         JOBS.append(fname)
         while fname not in COMPLETED_JOBS.keys():
             pass
         result = COMPLETED_JOBS[fname]
         del COMPLETED_JOBS[fname]
-        if os.path.exists(fname):
-            os.remove(fname)
+        # if os.path.exists(fname):
+        #     os.remove(fname)
+        print
         return result
 
 if __name__ == "__main__":
