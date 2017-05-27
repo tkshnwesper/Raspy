7b7c95e044613ac0dd8ad2fc3a81cdcde67f92ca
Nitin <nitinsrinivas@yahoo.com>
2017-05-27 18:49:27 +0530

    float

--- capture_thread.py
+++ capture_thread.py
@@ -16,6 +16,6 @@ class CaptureThread(threading.Thread):
         pname = PostName(post.response.text)
         print(pname.response.text)
         self.conn.item = pname.response.text[:len(pname.response.text)-4]
-        self.conn.accuracy = pname.response.text[len(pname.response.text)-3:]
+        self.conn.accuracy = float(pname.response.text[len(pname.response.text)-3:])
         if os.path.exists(loc):
             os.remove(loc)
